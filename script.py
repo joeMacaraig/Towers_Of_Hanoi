718b59ce2fa2bd6b006b86df2788b82722b5d660
Joseph Macaraig <bee@Josephs-MBP.lan>
2022-01-03 12:38:03 -0800

    Updated Files
    Updated the stack.py. Added a get_input function in script.py and declared some variables.

--- script.py
+++ script.py
@@ -1,3 +1,68 @@
 from stack import Stack
 
-#main
+stacks = []
+
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+
+#title
+print("\n")
+print("\n")
+print("\n")
+
+number_disks = int(input("\nHow many disks do you want to play with in this game?\n"))
+
+while number_disks < 3: 
+    number_disks = int(input("Enter a number GREATER than or EQUAL to 3.\n"))
+
+for num in range(number_disks, 0, -1):
+    num.push("Left")
+
+number_optimal_moves = 2 ** number_disks - 1
+num_user_moves = 0
+
+print("\nThe fastest you can solve this game is in {0} moves.".format(number_optimal_moves))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_input(): 
+    choices = [stack.get_name()[0] for stack in stacks]
+
+    while True: 
+
+        for i in range(len(stacks)):
+            name = stacks[i].get_name()
+            letter = choices[i]
+            print("Enter {0} for {1}".format(letter, name))
+
+        user_input = input("")
+
+        if user_input in choices: 
+        
+            for i in range(len(stacks)):
+
+                if user_input == choices[i]: 
+                    return stacks[i]
+        
--- stack.py
+++ stack.py
@@ -40,3 +40,12 @@ class Stack:
     
     def get_name(self):
         return self.name
+
+    def print_items(self):
+        pointer = self.top_item
+        print_list = []
+        while pointer: 
+            print_list.append(pointer.get_value())
+            pointer = pointer.get_next_node()
+        print_list.reverse()
+        print("{0} Stack: {1}".format(self.get_name(), print_list))
